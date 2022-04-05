bd95fb8c7114658b1a968a6262242f889c72972a
gbbreel <gabrielgallo1888@gmail.com>
2022-04-05 13:51:36 -0300

    Adicionando JavaScript

---
+++ index.js
@@ -0,0 +1,9 @@
+function test(){
+    
+    const name = document.getElementById('name').value
+    const mail = document.getElementById('mail').value
+    const area = document.getElementById('area').value
+
+    return alert(`wooooooooow \n \n Seja bem ${name} ao bootcamp da área de ${area}. \n \n Você se cadastrou  com o email \n ${mail}`)
+
+}
